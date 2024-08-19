a724105cee325f44ebb8143682f9570236df58e4
venomdarshu7 <138948660+venomdarshu7@users.noreply.github.com>
2024-08-19 08:45:18 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -9,9 +9,9 @@ class script(object):
 ★ Language: <a href=https://www.python.org>Python</a>
 ★ Library: <a href=https://pyrogram.org>Pyrogram</a>"""
 
-    MY_OWNER_TXT = """★ Name: HA Bots
-★ Username: @HA_Bots
-★ Country: Sri Lanka 🇱🇰"""
+    MY_OWNER_TXT = """★ Name: BOSS🎉
+<pre>★ Username: @KFCINEMAS
+★ Country: INDIA 🇱🇰</pre>"""
 
     STATUS_TXT = """🗂 Total Files: <code>{}</code>
 👤 Total Users: <code>{}</code>

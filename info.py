a25ed3791aecd333f22d5e90cb13655eedc56247
venomdarshu7 <138948660+venomdarshu7@users.noreply.github.com>
2024-08-19 08:05:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -119,7 +119,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
+URL = environ.get("URL", "proper-miguela-kfcinemas3-98043415.koyeb.app/")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()

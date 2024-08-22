aef5f33a351eafc0b5eda05975933679932ae77c
omyogesh695 <108509038+omyogesh695@users.noreply.github.com>
2024-08-22 21:30:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,7 +75,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mzfilestore01')
 SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/+Wvp5utJI42UzZmI5')
 OWNER_USERNAME = environ.get("OWNER_USERNAME", "https://t.me/sammarathi")
 UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/+Gw1YhDuUdNU1Mjll')
-FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/allMoviediscussion_1')
+FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/+Wvp5utJI42UzZmI5')
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/KFCINEMAS")
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/KFCINEMAS")
 

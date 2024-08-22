1960b01a32f92e90ecbfbf79e44063d795fd664d
omyogesh695 <108509038+omyogesh695@users.noreply.github.com>
2024-08-22 16:33:49 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -9,8 +9,8 @@ class script(object):
 ★ Language: <a href=https://www.python.org>Python</a>
 ★ Library: <a href=https://pyrogram.org>Pyrogram</a>"""
 
-    MY_OWNER_TXT = """★ Name: BOSS🎉
-<pre>★ Username: @KFCINEMAS
+    MY_OWNER_TXT = """★ Name: ℳℛ.ЅᎯℳ🎉
+<pre>★ Username: @sammarathi
 ★ Country: INDIA 🇱🇰</pre>"""
 
     STATUS_TXT = """🗂 Total Files: <code>{}</code>

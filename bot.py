fd5d13bf98963ca5f4e0f34a9ffc1fffa9c7180f
yba9285 <108516941+yba9285@users.noreply.github.com>
2025-03-14 00:45:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ uvloop.install()
 class Bot(Client):
     def __init__(self):
         super().__init__(
-            name='Auto_Filter_Bot',
+            name='Mz_Auto_Filter',
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,

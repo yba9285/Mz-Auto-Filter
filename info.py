f0b44b92b0afef6c31d714de2f9f47ad011319d3
yba9285 <108516941+yba9285@users.noreply.github.com>
2025-03-13 23:29:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -119,7 +119,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://lengthy-delilah-mzbotz-16a91cee.koyeb.app/")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
